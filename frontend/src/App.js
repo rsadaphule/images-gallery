49cd24858415434e57ab0446b487f13e33d8b502
Ravindra Sadaphule <rsadaphule@gmail.com>
2024-04-26 21:21:53 -0700

    added search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import { useState } from 'react';
+
 
 function App() {
+  const [ word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+
+    e.preventDefault();
+    console.log(word);
+  
+  }
+
+
+  console.log(word);
+
   return (
     <div className="App">
       <Header title="Images Gallery"/>
-      <Search></Search>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}></Search>
     </div>
   );
 }
+
+
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Row>
               <Col xs={9}>
-                <Form.Control placeholder="Search for new image..."></Form.Control>
+                <Form.Control  type="text"
+                               value={word}
+                               onChange={ (e) => setWord(e.target.value)} 
+                               placeholder="Search for new image..."></Form.Control>
               </Col>
               <Col>
                 <Button variant="primary" type="submit">Search</Button>
