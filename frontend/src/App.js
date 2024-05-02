c4275a1c8153249268ac33d7370bd5de5caef093
Ravindra Sadaphule <rsadaphule@gmail.com>
2024-05-01 22:23:28 -0700

    added liniting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,43 +3,40 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
-
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY ;
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [ word, setWord] = useState('');
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
-
     e.preventDefault();
+
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then( (res) => res.json() )
-    .then((data) => {
-        console.log(data)
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
         setWord('');
-
-    })
-    .catch((err)=> {
-      console.log(err);
-    })
-  
-  }
-
-
-
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   console.log(UNSPLASH_KEY);
 
   return (
     <div className="App">
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}></Search>
+      <Header title="Images Gallery" />
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
     </div>
   );
 }
-
-
 
 export default App;
